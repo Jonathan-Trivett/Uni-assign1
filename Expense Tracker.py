b4a653f698ededd2e6d0bc7c2f67ca859e609f1a
Jonathan Trivett <trivettjonathan@gmail.com>
2025-06-05 20:56:15 +0100

    Added opt to add, remove and view expenses

--- Expense Tracker.py
+++ Expense Tracker.py
@@ -4,3 +4,27 @@ class Expense:
         self.amount = amount
         self.description = description
         
+class ExpenseTracker:
+    def __init__(self):
+        self.expenses = []
+
+    def add_expense(self, expense):
+        self.expenses.append(expense)
+
+    def remove_expense(self, index):
+        if 0 <= index < len(self.expenses):
+            del self.expenses[index]
+            print("Expense removed successfully.")
+        else:
+            print("Invalid expense index.")
+
+    def view_expenses(self):
+        if len(self.expenses) == 0:
+            print("No expenses recorded.")
+        else:
+            print("Expense List:")
+            for i, expense in enumerate(self.expenses, start=1):
+                print(f"{i}. {expense.date} - {expense.amount} - {expense.description}")
+                
+
+    
